0baa8351559fc34370fec93a533af3728f80023b
Hyped <thathypedperson@gmail.com>
2025-12-02 19:27:27 -0800

    manually include all the assets in one file

--- src/ovl_En_Clear_Tag/gArwingDebrisEffectDL.inc.c
+++
@@ -1,3 +0,0 @@
-    gsSPVertex(&gArwingDebrisEffectVtx[0], 3, 0),
-    gsSP1Triangle(0, 1, 2, 0),
-    gsSPEndDisplayList(),
--- src/ovl_En_Clear_Tag/gArwingFireEffectDL.inc.c
+++
@@ -1,4 +0,0 @@
-    gsSPDisplayList(0x08000000),
-    gsSPVertex(&gArwingFireEffectVtx[0], 3, 0),
-    gsSP1Triangle(0, 1, 2, 0),
-    gsSPEndDisplayList(),
