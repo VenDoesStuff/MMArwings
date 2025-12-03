147ea6bfb79e0c6cd460b014693fa1a3a014174d
VenDoesStuff <VentusDoesStuff@gmail.com>
2025-12-02 19:53:39 -0700

    Added OOT arwing files

---
+++ src/ovl_En_Clear_Tag.h
@@ -0,0 +1,68 @@
+#ifndef OVL_EN_CLEAR_TAG_H
+#define OVL_EN_CLEAR_TAG_H
+
+#include "tex_len.h"
+#include "ultra64.h"
+
+static Gfx gArwingDL[91];
+static Vtx gArwingVtx[148];
+#define ovl_En_Clear_Tag_00000C18_Tex_WIDTH 8
+#define ovl_En_Clear_Tag_00000C18_Tex_HEIGHT 8
+extern u64 ovl_En_Clear_Tag_00000C18_Tex[TEX_LEN(u64, ovl_En_Clear_Tag_00000C18_Tex_WIDTH, ovl_En_Clear_Tag_00000C18_Tex_HEIGHT, 16)];
+#define ovl_En_Clear_Tag_00000C98_Tex_WIDTH 32
+#define ovl_En_Clear_Tag_00000C98_Tex_HEIGHT 32
+extern u64 ovl_En_Clear_Tag_00000C98_Tex[TEX_LEN(u64, ovl_En_Clear_Tag_00000C98_Tex_WIDTH, ovl_En_Clear_Tag_00000C98_Tex_HEIGHT, 16)];
+#define ovl_En_Clear_Tag_00001498_Tex_WIDTH 64
+#define ovl_En_Clear_Tag_00001498_Tex_HEIGHT 32
+extern u64 ovl_En_Clear_Tag_00001498_Tex[TEX_LEN(u64, ovl_En_Clear_Tag_00001498_Tex_WIDTH, ovl_En_Clear_Tag_00001498_Tex_HEIGHT, 16)];
+#define ovl_En_Clear_Tag_00002498_Tex_WIDTH 32
+#define ovl_En_Clear_Tag_00002498_Tex_HEIGHT 32
+extern u64 ovl_En_Clear_Tag_00002498_Tex[TEX_LEN(u64, ovl_En_Clear_Tag_00002498_Tex_WIDTH, ovl_En_Clear_Tag_00002498_Tex_HEIGHT, 16)];
+#define ovl_En_Clear_Tag_00002C98_Tex_WIDTH 32
+#define ovl_En_Clear_Tag_00002C98_Tex_HEIGHT 32
+extern u64 ovl_En_Clear_Tag_00002C98_Tex[TEX_LEN(u64, ovl_En_Clear_Tag_00002C98_Tex_WIDTH, ovl_En_Clear_Tag_00002C98_Tex_HEIGHT, 16)];
+#define ovl_En_Clear_Tag_00003498_Tex_WIDTH 32
+#define ovl_En_Clear_Tag_00003498_Tex_HEIGHT 32
+extern u64 ovl_En_Clear_Tag_00003498_Tex[TEX_LEN(u64, ovl_En_Clear_Tag_00003498_Tex_WIDTH, ovl_En_Clear_Tag_00003498_Tex_HEIGHT, 16)];
+static Gfx gArwingLaserDL[14];
+static Vtx gArwingLaserVtx[6];
+#define ovl_En_Clear_Tag_00003D68_Tex_WIDTH 16
+#define ovl_En_Clear_Tag_00003D68_Tex_HEIGHT 16
+extern u64 ovl_En_Clear_Tag_00003D68_Tex[TEX_LEN(u64, ovl_En_Clear_Tag_00003D68_Tex_WIDTH, ovl_En_Clear_Tag_00003D68_Tex_HEIGHT, 16)];
+static Vtx gArwingBackfireVtx[3];
+static Gfx gArwingBackfireDL[16];
+#define ovl_En_Clear_Tag_00004018_Tex_WIDTH 16
+#define ovl_En_Clear_Tag_00004018_Tex_HEIGHT 16
+extern u64 ovl_En_Clear_Tag_00004018_Tex[TEX_LEN(u64, ovl_En_Clear_Tag_00004018_Tex_WIDTH, ovl_En_Clear_Tag_00004018_Tex_HEIGHT, 8)];
+#define ovl_En_Clear_Tag_00004118_Tex_WIDTH 16
+#define ovl_En_Clear_Tag_00004118_Tex_HEIGHT 16
+extern u64 ovl_En_Clear_Tag_00004118_Tex[TEX_LEN(u64, ovl_En_Clear_Tag_00004118_Tex_WIDTH, ovl_En_Clear_Tag_00004118_Tex_HEIGHT, 16)];
+static Vtx gArwingDebrisEffectVtx[3];
+static Gfx gArwingDebrisEffectMaterialDL[16];
+static Gfx gArwingDebrisEffectDL[3];
+#define ovl_En_Clear_Tag_000043E0_Tex_WIDTH 32
+#define ovl_En_Clear_Tag_000043E0_Tex_HEIGHT 64
+extern u64 ovl_En_Clear_Tag_000043E0_Tex[TEX_LEN(u64, ovl_En_Clear_Tag_000043E0_Tex_WIDTH, ovl_En_Clear_Tag_000043E0_Tex_HEIGHT, 4)];
+#define ovl_En_Clear_Tag_000047E0_Tex_WIDTH 32
+#define ovl_En_Clear_Tag_000047E0_Tex_HEIGHT 32
+extern u64 ovl_En_Clear_Tag_000047E0_Tex[TEX_LEN(u64, ovl_En_Clear_Tag_000047E0_Tex_WIDTH, ovl_En_Clear_Tag_000047E0_Tex_HEIGHT, 4)];
+static Vtx gArwingFireEffectVtx[3];
+static Gfx gArwingFireEffectMaterialDL[21];
+static Gfx gArwingFireEffectDL[4];
+#define ovl_En_Clear_Tag_00004AD8_Tex_WIDTH 64
+#define ovl_En_Clear_Tag_00004AD8_Tex_HEIGHT 64
+extern u64 ovl_En_Clear_Tag_00004AD8_Tex[TEX_LEN(u64, ovl_En_Clear_Tag_00004AD8_Tex_WIDTH, ovl_En_Clear_Tag_00004AD8_Tex_HEIGHT, 8)];
+static Vtx gArwingFlashEffectVtx[4];
+static Gfx gArwingFlashEffectDL[16];
+#define ovl_En_Clear_Tag_00005B98_Tex_WIDTH 32
+#define ovl_En_Clear_Tag_00005B98_Tex_HEIGHT 32
+extern u64 ovl_En_Clear_Tag_00005B98_Tex[TEX_LEN(u64, ovl_En_Clear_Tag_00005B98_Tex_WIDTH, ovl_En_Clear_Tag_00005B98_Tex_HEIGHT, 4)];
+static Vtx gArwingFlashEffectGroundVtx[3];
+static Gfx gArwingFlashEffectGroundDL[17];
+#define ovl_En_Clear_Tag_00005E50_Tex_WIDTH 32
+#define ovl_En_Clear_Tag_00005E50_Tex_HEIGHT 32
+extern u64 ovl_En_Clear_Tag_00005E50_Tex[TEX_LEN(u64, ovl_En_Clear_Tag_00005E50_Tex_WIDTH, ovl_En_Clear_Tag_00005E50_Tex_HEIGHT, 8)];
+static Vtx gArwingShadowVtx[3];
+static Gfx gArwingShadowDL[16];
+
+#endif
